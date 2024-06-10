ae52238fc1e1ebbfb1af126c9f3ab70915786ae5
c23005 <c23005@std.it-college.ac.jp>
2024-06-10 11:15:56 +0900

    箱の生成プログラムを簡素にした

--- ProjectData/Pinnkudama/Assets/Scripts/KaabilScript.cs
+++ ProjectData/Pinnkudama/Assets/Scripts/KaabilScript.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -21,8 +22,8 @@ public class KaabilScript : MonoBehaviour
 
     void Start()
     {
-        Seiti();
         life = DefaltLife;
+        Seiti();
     }
 
     // Update is called once per frame
@@ -76,68 +77,14 @@ public class KaabilScript : MonoBehaviour
 
     public void Seiti()
     {
-        GameObject Box1 = Instantiate(colorBox[0], new Vector3(3.5f, 0.0f, 3.5f), Quaternion.identity);
-        GameObject Box2 = Instantiate(colorBox[1], new Vector3(2.0f, 0.0f, 3.5f), Quaternion.identity);
-        GameObject Box3 = Instantiate(colorBox[2], new Vector3(0.5f, 0.0f, 3.5f), Quaternion.identity);
-        GameObject Box4 = Instantiate(colorBox[1], new Vector3(-1.0f, 0.0f, 3.5f), Quaternion.identity);
-        GameObject Box5 = Instantiate(colorBox[0], new Vector3(-2.5f, 0.0f, 3.5f), Quaternion.identity);
-        GameObject Box6 = Instantiate(colorBox[2], new Vector3(-4.0f, 0.0f, 3.5f), Quaternion.identity);
-        GameObject Box7 = Instantiate(colorBox[0], new Vector3(3.5f, 0.0f, 2.0f), Quaternion.identity);
-        GameObject Box8 = Instantiate(colorBox[1], new Vector3(2.0f, 0.0f, 2.0f), Quaternion.identity);
-        GameObject Box9 = Instantiate(colorBox[2], new Vector3(0.5f, 0.0f, 2.0f), Quaternion.identity);
-        GameObject Box10 = Instantiate(colorBox[1], new Vector3(-1.0f, 0.0f, 2.0f), Quaternion.identity);
-        GameObject Box11 = Instantiate(colorBox[0], new Vector3(-2.5f, 0.0f, 2.0f), Quaternion.identity);
-        GameObject Box12 = Instantiate(colorBox[2], new Vector3(-4.0f, 0.0f, 2.0f), Quaternion.identity);
-        GameObject Box13 = Instantiate(colorBox[0], new Vector3(3.5f, 0.0f, 0.5f), Quaternion.identity);
-        GameObject Box14 = Instantiate(colorBox[1], new Vector3(2.0f, 0.0f, 0.5f), Quaternion.identity);
-        GameObject Box15 = Instantiate(colorBox[2], new Vector3(0.5f, 0.0f, 0.5f), Quaternion.identity);
-        GameObject Box16 = Instantiate(colorBox[1], new Vector3(-1.0f, 0.0f, 0.5f), Quaternion.identity);
-        GameObject Box17 = Instantiate(colorBox[0], new Vector3(-2.5f, 0.0f, 0.5f), Quaternion.identity);
-        GameObject Box18 = Instantiate(colorBox[2], new Vector3(-4.0f, 0.0f, 0.5f), Quaternion.identity);
-        GameObject Box19 = Instantiate(colorBox[0], new Vector3(3.5f, 0.0f, -1.0f), Quaternion.identity);
-        GameObject Box20 = Instantiate(colorBox[1], new Vector3(2.0f, 0.0f, -1.0f), Quaternion.identity);
-        GameObject Box21 = Instantiate(colorBox[2], new Vector3(0.5f, 0.0f, -1.0f), Quaternion.identity);
-        GameObject Box22 = Instantiate(colorBox[1], new Vector3(-1.0f, 0.0f, -1.0f), Quaternion.identity);
-        GameObject Box23 = Instantiate(colorBox[0], new Vector3(-2.5f, 0.0f, -1.0f), Quaternion.identity);
-        GameObject Box24 = Instantiate(colorBox[2], new Vector3(-4.0f, 0.0f, -1.0f), Quaternion.identity);
-        GameObject Box25 = Instantiate(colorBox[0], new Vector3(3.5f, 0.0f, -2.5f), Quaternion.identity);
-        GameObject Box26 = Instantiate(colorBox[1], new Vector3(2.0f, 0.0f, -2.5f), Quaternion.identity);
-        GameObject Box27 = Instantiate(colorBox[2], new Vector3(0.5f, 0.0f, -2.5f), Quaternion.identity);
-        GameObject Box28 = Instantiate(colorBox[1], new Vector3(-1.0f, 0.0f, -2.5f), Quaternion.identity);
-        GameObject Box29 = Instantiate(colorBox[0], new Vector3(-2.5f, 0.0f, -2.5f), Quaternion.identity);
-        GameObject Box30 = Instantiate(colorBox[2], new Vector3(-4.0f, 0.0f, -2.5f), Quaternion.identity);
-        
-        Box1.transform.parent = boxs.transform;
-        Box2.transform.parent = boxs.transform;
-        Box3.transform.parent = boxs.transform;
-        Box4.transform.parent = boxs.transform;
-        Box5.transform.parent = boxs.transform;
-        Box6.transform.parent = boxs.transform;
-        Box7.transform.parent = boxs.transform;
-        Box8.transform.parent = boxs.transform;
-        Box9.transform.parent = boxs.transform;
-        Box10.transform.parent = boxs.transform;
-        Box11.transform.parent = boxs.transform;
-        Box12.transform.parent = boxs.transform;
-        Box13.transform.parent = boxs.transform;
-        Box14.transform.parent = boxs.transform;
-        Box15.transform.parent = boxs.transform;
-        Box16.transform.parent = boxs.transform;
-        Box17.transform.parent = boxs.transform;
-        Box18.transform.parent = boxs.transform;
-        Box19.transform.parent = boxs.transform;
-        Box20.transform.parent = boxs.transform;
-        Box21.transform.parent = boxs.transform;
-        Box22.transform.parent = boxs.transform;
-        Box23.transform.parent = boxs.transform;
-        Box24.transform.parent = boxs.transform;
-        Box25.transform.parent = boxs.transform;
-        Box26.transform.parent = boxs.transform;
-        Box27.transform.parent = boxs.transform;
-        Box28.transform.parent = boxs.transform;
-        Box29.transform.parent = boxs.transform;
-        Box30.transform.parent = boxs.transform;
-
+        for(int i = 0; i < 30;  i++)
+        {
+            int colorNum = UnityEngine.Random.Range(0, 3);
+            float Xpos = 3.5f - ((i % 6) * 1.5f);
+            float Zpos = 3.5f - (MathF.Floor(i / 6) * 1.5f);
+            GameObject Box = Instantiate(colorBox[colorNum], new Vector3(Xpos, 0,Zpos), Quaternion.identity); 
+            Box.transform.parent = boxs.transform;
+        }
     }
 
 }
